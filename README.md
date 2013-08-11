4a1e0295090b8eabc740c2e6b2fff56a100f1135
Leo Deng <myst.dg@gmail.com>
2013-08-12 00:01:04 +0800

    game of life

---
+++ README.md
@@ -0,0 +1,33 @@
+Conway's Game of Life
+---------------------
+
+**About this repo**
+
++ Conway's Game of Life written in JavaScript.
++ This project is released under the [MIT license](http://opensource.org/licenses/MIT).
+
+
+**About the game**
+
++ The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
++ The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.
++ One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+
+Rules:
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
++ This is not a funny game. This is about life and death.
+
+
+**Read the blog post**
+
++ Not written yet.
+
+
+**Try out the demo**
+
++ http://myst729.github.io/game-of-life/
---
+++ gol.js
@@ -0,0 +1,122 @@
+void function(window, document, undefined) {
+
+  // ES5 strict mode
+  "use strict";
+
+  var UNIT = 3;   // cell width and height
+  var SIDE = 200; // number of cells vertically and horizontally
+
+  var state = [];
+  var flag = false;
+  var cells;
+  var random = document.getElementById('random');
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var step = document.getElementById('step');
+
+  // Cross-browser compatible event handler.
+  var addEvent = function(element, type, handler) {
+    if(element.addEventListener) {
+      addEvent = function(element, type, handler) {
+        element.addEventListener(type, handler, false);
+      };
+    } else if(element.attachEvent) {
+      addEvent = function(element, type, handler) {
+        element.attachEvent('on' + type, handler);
+      };
+    } else {
+      addEvent = function(element, type, handler) {
+        element['on' + type] = handler;
+      };
+    }
+    addEvent(element, type, handler);
+  };
+
+  // Calculate the next generation of cells and draw them.
+  var spawn = function() {
+    var next = [];
+    var neighbors = 0;
+    for(var i = 0; i < SIDE + 2; i++) {
+      next[i] = [];
+      for(var j = 0; j < SIDE + 2; j++) {
+        if(i === 0 || j === 0 || i === SIDE+1 || j === SIDE+1) {
+          // shim elements
+          next[i][j] = 0;
+        } else {
+          neighbors = state[i-1][j-1] + state[i-1][j] + state[i-1][j+1] + state[i][j-1] + state[i][j+1] + state[i+1][j-1] + state[i+1][j] + state[i+1][j+1];
+          if(state[i][j] === 0 && neighbors === 3) {
+            // this cell is dead, but will be alive in next round
+            next[i][j] = 1;
+          } else if(state[i][j] === 1 && neighbors > 1 && neighbors < 4) {
+            // this cell is alive, and will keep alive in next round
+            next[i][j] = 1;
+          } else {
+            // dead cell in next round
+            next[i][j] = 0;
+          }
+        }
+      }
+    }
+    state = next;
+    draw();
+  };
+
+  // Draw the cells in the canvas.
+  // TODO: Use CANVAS instead of DIVs.
+  var draw = function() {
+    var index;
+    for(var i = 1; i < SIDE + 1; i++) {
+      for(var j = 1; j < SIDE + 1; j++) {
+        index = SIDE * (i-1) + (j-1);
+        cells[index].className = (state[i][j] === 1) ? 'alive' : '';
+      }
+    }
+    if(flag) {
+      setTimeout(spawn, 50);
+    }
+  };
+
+  // Generate random initial cells and draw them.
+  var randomize = function() {
+    flag = false;
+    for(var i = 1; i < SIDE + 1; i++) {
+      for(var j = 1; j < SIDE + 1; j++) {
+        state[i][j] = (Math.random() < 0.8) ? 0 : 1;
+      }
+    }
+    draw();
+  };
+
+  // Start to evolution.
+  var startGame = function() {
+    flag = true;
+    spawn();
+  };
+
+  // Stop evolution.
+  var stopGame = function() {
+    flag = false;
+  };
+
+  // Initialize the canvas.
+  // TODO: Use CANVAS instead of DIVs.
+  var init = function() {
+    var gol = document.getElementById('gol');
+    gol.style.width = gol.style.height = UNIT * SIDE + 'px';
+    gol.innerHTML = (new Array(SIDE * SIDE + 1)).join('<span></span>');
+    cells = gol.children;
+    for(var i = 0; i < SIDE + 2; i++) {
+      state[i] = [];
+      for(var j = 0; j < SIDE + 2; j++) {
+        state[i][j] = 0;
+      }
+    }
+  };
+
+  addEvent(random, 'click', randomize);
+  addEvent(start, 'click', startGame);
+  addEvent(stop, 'click', stopGame);
+  addEvent(step, 'click', spawn);
+  addEvent(window, 'load', init);
+
+}(window, document);
